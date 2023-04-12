e77509a0dc0be63b9ff84c8b0fe1995fa3f016bd
George <GeorgeGreen.QA@yandex.ru>
2023-04-12 12:59:19 +0400

    added euro and actual currences

--- dictionary/lexicon_ru.py
+++ dictionary/lexicon_ru.py
@@ -5,12 +5,14 @@ STATES['chosen_currency'] = 'gel'
 CURRENCIES: dict[str, str] = {
                               'dollar': 'Доллар',
                               'gel': 'Лари',
-                              'drum': 'Драмы'
+                              'drum': 'Драмы',
+                              'eur': 'Eвро',
                                }
 
 
 LEXICON_RU: dict[str, str] = {'/start': f'Введите число для перевода в {CURRENCIES[STATES["chosen_currency"]]}',
                               '/help': '/хелп',
                               'currency': 'Выбор валюты',
+                              'currency_actual': 'Курсы валют',
                               }
 
